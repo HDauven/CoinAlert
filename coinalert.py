f442b316816c5604339f7818b453675a9bde8b71
Hein Dauven <heindauven@gmail.com>
2018-01-02 01:16:37 +0100

    Add support for Binance, simplify compared lists, extract functionality in separate function.

--- coinalert.py
+++ coinalert.py
@@ -1,60 +1,83 @@
+from binance.client import Client
 from bittrex.bittrex import Bittrex
 from deepdiff import DeepDiff
 from time import time, sleep
 import dotenv
 import pprint
+import datetime
 from telegram.ext import Updater, CommandHandler
-import logging
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
 
 pp = pprint.PrettyPrinter(width=41, compact=True)
 
 dotenv.load()
+
+binance_api_key = dotenv.get('binance_api_key')
+binance_api_secret = dotenv.get('binance_api_secret')
+
+binance_client = Client(binance_api_key, binance_api_secret)
+tickers = binance_client.get_all_tickers()
+t1 = [{'symbol': ticker['symbol']} for ticker in tickers]
 
 bittrex_api_key = dotenv.get('bittrex_api_key')
 bittrex_api_secret = dotenv.get('bittrex_api_secret')
 interval = dotenv.get('polling_interval')
 
 my_bittrex = Bittrex(bittrex_api_key, bittrex_api_secret)
-
-markets = my_bittrex.get_markets()['result']
 currencies = my_bittrex.get_currencies()['result']
+c1 = [{'Currency': currency['Currency'], 'CurrencyLong': currency['CurrencyLong']} for currency in currencies]
+markets = my_bittrex.get_markets()['result']
+m1 = [{'MarketName': market['MarketName'], 'MarketCurrencyLong': market['MarketCurrencyLong']} for market in markets]
 
 updater = Updater(token=dotenv.get('telegram_bot'))
 dispatcher = updater.dispatcher
 
-pp.pprint(len(markets))
+dispatcher.bot.send_message(dotenv.get('telegram_group'), 'Waking up...')
+
+dispatcher.bot.send_message(dotenv.get('telegram_group'), '{} tickers on Binance'.format(len(tickers)))
 dispatcher.bot.send_message(dotenv.get('telegram_group'), '{} markets on Bittrex'.format(len(markets)))
-pp.pprint(len(currencies))
 dispatcher.bot.send_message(dotenv.get('telegram_group'), '{} currencies on Bittrex'.format(len(currencies)))
 
 
-while True:
-    print('Tick')
-    temp_markets = my_bittrex.get_markets()['result']
-    temp_currencies = my_bittrex.get_currencies()['result']
-    new_markets = DeepDiff(markets, temp_markets, ignore_order=True)
-    new_currencies = DeepDiff(currencies, temp_currencies, ignore_order=True)
-    if 'iterable_item_added' in new_markets:
-        print('New market added!')
-        dispatcher.bot.send_message(dotenv.get('telegram_group'), 'New market(s) added to Bittrex!')
-        for market in new_markets['iterable_item_added']:
-            print("New market: {}".format(market))
-            dispatcher.bot.send_message(dotenv.get('telegram_group'), 'New market: {}'.format(market))
+def check_for_changes(command, result_list, value_of_interest, value_of_interest_long, identifier, exchange):
+    if command in result_list:
+        print('Changes found on {}'.format(exchange))
+        for item in result_list[command]:
+            if not value_of_interest_long:
+                message = 'New {} found on {}: {}'.format(identifier, exchange, result_list[command][item][value_of_interest])
+                print(message)
+                dispatcher.bot.send_message(dotenv.get('telegram_group'), message)
+            else:
+                message = 'New {} found on {}: {} - {}'.format(identifier, exchange,
+                                                           result_list[command][item][value_of_interest],
+                                                           result_list[command][item][value_of_interest_long])
+                print(message)
+                dispatcher.bot.send_message(dotenv.get('telegram_group'), message)
 
-    if 'iterable_item_added' in new_currencies:
-        print('New currency added!')
-        dispatcher.bot.send_message(dotenv.get('telegram_group'), 'New currency(ies) added to Bittrex!')
-        for currency in new_currencies['iterable_item_added']:
-            print("New currency: {}".format(currency))
-            dispatcher.bot.send_message(dotenv.get('telegram_group'), 'New currency: {}'.format(currency))
+
+while True:
+    print('Checked on {}'.format(datetime.datetime.now()))
+    temp_currencies = my_bittrex.get_currencies()['result']
+    c2 = [{'Currency': currency['Currency'], 'CurrencyLong': currency['CurrencyLong']} for currency in temp_currencies]
+    temp_markets = my_bittrex.get_markets()['result']
+    m2 = [{'MarketName': market['MarketName'], 'MarketCurrencyLong': market['MarketCurrencyLong']} for market in temp_markets]
+    temp_tickers = binance_client.get_all_tickers()
+    t2 = [{'symbol': ticker['symbol']} for ticker in temp_tickers]
+
+    new_currencies = DeepDiff(c1, c2, ignore_order=True)
+    new_markets = DeepDiff(m1, m2, ignore_order=True)
+    new_tickers = DeepDiff(t1, t2, ignore_order=True)
+
+    check_for_changes('iterable_item_added', new_currencies, 'Currency', 'CurrencyLong', 'currency', 'Bittrex')
+    check_for_changes('iterable_item_added', new_markets, 'MarketName', 'MarketCurrencyLong', 'market', 'Bittrex')
+    check_for_changes('iterable_item_added', new_tickers, 'symbol', '', 'ticker', 'Binance')
+
+    c1 = c2
+    m1 = m2
+    t1 = t2
 
     pp.pprint(new_markets)
     pp.pprint(new_currencies)
+    pp.pprint(new_tickers)
     sleep(interval - time() % interval)
 
 # def main():
